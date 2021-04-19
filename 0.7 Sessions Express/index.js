2e8ea45ca360a4c816812a0cc15f84a86b98d49b
Lars Flieger <a.purenetwork@gmail.com>
2021-04-19 13:10:28 +0200

    Update

--- 0.7 Sessions Express/index.js
+++ 0.7 Sessions Express/index.js
@@ -3,8 +3,11 @@ const session = require('express-session')
 
 const PORT = 3100
 const RATE_LIMIT = 10
+const TIME_LIMIT = 5000
 
 const app = express()
+
+// Cookie middleware
 app.use(session({
     secret: 'ygRdgzWJeiv8Bndub0cgi6ykH8odeoNDRqVRaVv2jJzGVmhCtunPO58OQTSVwyk',
     resave: false,
@@ -12,23 +15,33 @@ app.use(session({
     cookie: { maxAge: 60000 }  
 }))
 
+// Time check middleware
 app.use((req, res, next) => {
-    if(req.session.counter)
-        req.session.counter++
-    else
-        req.session.counter = 1
-    
-    next()
+    if(req.session.time && Date.now() - req.session.time < TIME_LIMIT) {
+        res.send({
+            message: 'You are to fast... Wait: ' + (TIME_LIMIT - (Date.now() - req.session.time))/1000 + ' Seconds'
+        }).status(405)
+    } else {
+        req.session.time = Date.now()
+        next()
+    }
 })
 
+// Counter middleware
 app.use((req, res, next) => {
-    if(req.session.counter > RATE_LIMIT)
+    if(!req.session.counter)
+        req.session.counter = 1
+
+    console.log(req.session.counter)
+    if(req.session.counter >= RATE_LIMIT)
         res.send({
             message: 'Limit reached',
             counter: req.session.counter
         }).status(405)
-    else
+    else {
+        req.session.counter++
         next()
+    } 
 })
 
 app.get('/', (req, res) => {
