e2a5ed4a1588174e9c3f9ff82482f69822c79740
Lars Flieger <a.purenetwork@gmail.com>
2021-04-19 12:02:20 +0200

    Add static resources

--- 0.6 EJS Training/index.js
+++ 0.6 EJS Training/index.js
@@ -5,6 +5,7 @@ let firstLoggingSent = false
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
 
 const DATA = [
     { id: 1, name: 'Apfel', color: 'gelb,rot' },
